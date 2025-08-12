e40fbe84d6d32403764986dea21a4d5ea1df7671
Rushikesh <tamberushikesh450@gmail.com>
2025-08-12 23:29:10 +0530

    add assignment

--- Day 26-07-2025/Exercise/Day6_Training/src/com/wipro/test/StringLowerCaseTest.java
+++ Day 26-07-2025/Exercise/Day6_Training/src/com/wipro/test/StringLowerCaseTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class StringLowerCaseTest {
 
 	@ParameterizedTest
-    @ValueSource(strings = {"shubham", "sham", "jayanth"})
+    @ValueSource(strings = {"Rushikesh", "sham", "jayanth"})
     void check(String s) {
         assertTrue(isToCheck(s));
     }
